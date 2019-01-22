05ff91a3254d5d919d3eec41dae9283a1bea2377
Euan Macqueen <2425291m@student.gla.ac.uk>
2019-01-22 13:26:25 +0000

    Up to page 63

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
 	name=models.CharField(max_length=128, unique=True)
-
+	likes=models.IntegerField(default=0)
+	views=models.IntegerField(default=0)
+	
+	class Meta:
+		verbose_name_plural = 'Categories'
+		
 	#may be able to remove 'self' due to Python version being > 3.0
 	def __str__(self):
 		return self.name
