90834197a3218a5736d489f4127a12ba49b756a8
euanM108 <euan_M108@hotmail.com>
2019-02-05 21:39:36 +0000

    pushing after force push deleted commits

--- rango/forms.py
+++ rango/forms.py
@@ -27,18 +27,17 @@ class PageForm(forms.ModelForm):
         if url and not url.startswith('http://'):
             url = 'http://' + url
             cleaned_data['url'] = url
+        return cleaned_data
 
-            return cleaned_data
 
-
-class Meta:
-    # Provide an association between the ModelForm and a models
-    model = Page
-    # What fields do we want to include in our form?
-    # This way we don't need every field in the model present.
-    # Some fields may allow NULL values, so we may not want to include them.
-    # Here, we are hiding the foreign key.
-    # we can either exclude the category field from the form,
-    exclude = ('category',)
-    # or specify the fields to include(i.e. not include the category field)
-    # field = ('title', 'url', 'views')
+    class Meta:
+        # Provide an association between the ModelForm and a models
+        model = Page
+        # What fields do we want to include in our form?
+        # This way we don't need every field in the model present.
+        # Some fields may allow NULL values, so we may not want to include them.
+        # Here, we are hiding the foreign key.
+        # we can either exclude the category field from the form,
+        exclude = ('category',)
+        # or specify the fields to include(i.e. not include the category field)
+        # field = ('title', 'url', 'views')
